2c195973c920480c77461aca120ca34220138052
Alex Perry <alexperry@Alexs-MacBook-Air.local>
2025-07-01 20:13:17 -0700

    Made some more progress!

--- Chapter1.DataVisualization.R
+++ Chapter1.DataVisualization.R
@@ -13,9 +13,11 @@ library(ggthemes)
 
 
 #1.2 First Steps 
+
 #How do we create visualizations to answer questions about patterns within penguins?
 
 #1.2.1 The penguins data frame 
+
 #A data frame is a rectangular collection of variables (columns) and observations (the rows)
 #penguins, the package, contains 344 observations
 
@@ -25,4 +27,53 @@ library(ggthemes)
 #tabular data = set of values, each associated with a variable and an observation.
 #tabular data is considered tidy if each value is placed in its own "cell" and each variable in its own column and each observations in its own row. 
 
-p
+#Creating a ggplot
+
+ggplot(data = penguins) #the data = penguins tells it what data to use. 
+
+#next, we have to add aesthetics using the mapping argument, which is defined in the aes() function, and the x and y arguments specify the variables to match them at. 
+
+ggplot(
+  data = penguins, 
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+)
+
+#at this point, the axis are defined, but we haven't specified how to exactly display the data or represent the observations. to do this, we use a "geom", the geometrical object that will represent a point.
+#plots are referred to by the different geom that the plot uses.
+#bar chart geom_bar() 
+#line chart geom_line()
+#boxplot uses geom_boxplot()
+#scatterplot is geom_point()
+
+ggplot(
+  data = penguins,
+  mapping = aes( x = flipper_length_mm, y = body_mass_g)
+) + 
+  geom_point()
+
+
+#1.2.4 Adding aesthetics and layers 
+
+#scatterplots are useful for observing relationships between two numerical variables, but we need to see if any other variables explain or change this observed relationship. the book says
+#that we should do this by representing the different species with different colored points. 
+
+ggplot(
+  data = penguins, 
+  mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species)
+) + geom_point()
+
+#the process of scaling is when the ggplot program matches a unique value of the aesthetic (here it's a color, to a unique level of the variable) it'll also add a legend when needed. 
+
+#the book says to add another layer. we're going to insert a smooth curve that displays the relationship between 
+#body mass and flipper length. the new geom is called geom_smooth(). and we'll specify that we want a linear model
+#using lm
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species)
+) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+# so if you do it like this, the geom arguments that follow will apply to each thing that's specified in the mapping
+# argument
